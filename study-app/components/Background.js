1f6c1d61129d5570fe9313a61c6ffa3e11174e41
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-22 13:44:16 +0200

    eslint fixes

--- study-app/components/Background.js
+++ study-app/components/Background.js
@@ -8,11 +8,11 @@ export default function Background()
         <video className='absolute top-0 -z-10 opacity-30 left-0 w-full h-full object-cover' autoPlay loop muted>
             <source src='mp4/filters/vhs.mp4' type='video/mp4' />
         </video>
-        {controlState.transitionPlaying && (<video key={controlState.transitionPlaying} muted className='absolute -z-20 top-0 left-0 w-full h-full object-cover' autoPlay loop muted>
+        {controlState.transitionPlaying && (<video key={controlState.transitionPlaying} muted className='absolute -z-20 top-0 left-0 w-full h-full object-cover' autoPlay loop>
             <source src='mp4/filters/transition.mp4' type='video/mp4' />
         </video>)}
         
-        <video key={controlState.background} muted className='absolute -z-30 top-0 left-0 w-full h-full object-cover' autoPlay loop muted>
+        <video key={controlState.background} muted className='absolute -z-30 top-0 left-0 w-full h-full object-cover' autoPlay loop>
             <source src={controlState.background} type='video/mp4' />
         </video>
     </div>)
