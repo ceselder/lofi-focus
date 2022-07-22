96d72aac1ef89ca4e2e90a293a75c2822991512d
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-22 17:59:35 +0200

    lets not use react player actually

--- study-app/components/Background.js
+++ study-app/components/Background.js
@@ -3,31 +3,21 @@ import { controlStateContext } from "../pages"
 import dynamic from "next/dynamic"
 const ReactPlayer = dynamic(() => import('react-player/lazy'), {
     ssr: false,
-})
+  })
 
-export default function Background() {
+export default function Background()
+{
     const [controlState, setControlState] = useContext(controlStateContext)
     return (<div>
-        <ReactPlayer
-            className='absolute top-0 -z-10 opacity-30 left-0 w-full h-full object-cover'
-            playing={true}
-            muted={true}
-            loop={true}
-            url='mp4/filters/vhs.mp4'
-        />
-        {controlState.transitionPlaying && (<ReactPlayer
-            className='absolute -z-20 top-0 left-0 w-full h-full object-cover'
-            playing={true}
-            muted={true}
-            loop={true}
-            url='mp4/filters/vhs.mp4'
-        />)}
-        <ReactPlayer
-            className='absolute -z-30 top-0 left-0 w-full h-full object-cover'
-            playing={true}
-            loop={true}
-            muted={true}
-            url={controlState.background}
-        />
+        <video className='absolute top-0 -z-10 opacity-30 left-0 w-full h-full object-cover' autoPlay loop muted>
+            <source src='mp4/filters/vhs.mp4' type='video/mp4' />
+        </video>
+        {controlState.transitionPlaying && (<video key={controlState.transitionPlaying} muted className='absolute -z-20 top-0 left-0 w-full h-full object-cover' autoPlay loop>
+            <source src='mp4/filters/transition.mp4' type='video/mp4' />
+        </video>)}
+        
+        <video key={controlState.background} muted className='absolute -z-30 top-0 left-0 w-full h-full object-cover' autoPlay loop>
+            <source src={controlState.background} type='video/mp4' />
+        </video>
     </div>)
 }
