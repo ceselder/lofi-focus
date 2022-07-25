d8fe03793252dbef8ff24ca4ab1a18455f822693
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-07-25 11:55:40 +0200

    mouse holding

---
+++ hooks/useMouseHold.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+
+export function useMouseHold(func, holdTime = 500, fireInterval = 100) {
+    const [mouseDown, setMouseDown] = useState(false)
+    const [holdCancelled, setHoldCancelled] = useState(false)
+    const [mouseHeld, setMouseHeld] = useState(false)
+
+    const mouseDownFunction = () => { setMouseDown(true); }
+    const mouseUpFunction = () => { setHoldCancelled(true); setMouseDown(false); }
+
+    useEffect(() => {
+        if (mouseDown) {
+            func()
+            const timeout = setTimeout(() => {
+                setMouseDown(newMouseDown => {
+                    if (newMouseDown) {
+                        setMouseHeld(true)
+                    }
+                    return newMouseDown
+                })
+                return clearTimeout(timeout)
+            }, holdTime)
+            useEffect(() => { //double register fix
+                if (holdCancelled)
+                {
+                    clearTimeout(timeout)
+                }
+            }, [holdCancelled])
+        }
+        else {
+            setMouseHeld(false)
+        }
+    }, [mouseDown])
+
+    useEffect(() => {
+        if (mouseHeld) {
+            const interval = setInterval(() => {
+                func()
+                setMouseHeld(held => {
+                    if (!held) {
+                        return clearInterval(interval)
+                    }
+                    return held
+                })
+            }, fireInterval)
+        }
+    }, [mouseHeld])
+
+    return [mouseDownFunction, mouseUpFunction]
+}
